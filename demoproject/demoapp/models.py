8e90389bef2109ec87f4b74c713bc764da9bdcae
swapnil-agashe <swapnil.agashe@coupa.com>
2024-04-16 18:07:41 +0530

    added migrations

--- demoproject/demoapp/models.py
+++ demoproject/demoapp/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from unicodedata import name
 # Create your models here.
 class Menu(models.Model):
-    name = models.CharField(max_length=50)
+    item_name = models.CharField(max_length=50)
     cuisine = models.CharField(max_length=100)
     price = models.IntegerField()
+    rating = models.IntegerField()
     
     def __str__(self):
         return self.name+" : " + self.cuisine
