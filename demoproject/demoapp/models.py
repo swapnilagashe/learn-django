9407ed10d3e3baec3e4ee2bf597242e7ab5cbac1
swapnil-agashe <swapnil.agashe@coupa.com>
2024-04-16 17:26:42 +0530

    added views and models basic exmple code

--- demoproject/demoapp/models.py
+++ demoproject/demoapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from unicodedata import name
 # Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=50)
+    cuisine = models.CharField(max_length=100)
+    price = models.IntegerField()
+    
+    def __str__(self):
+        return self.name+" : " + self.cuisine
--- demoproject/demoapp/views.py
+++ demoproject/demoapp/views.py
@@ -2,4 +2,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world, this is index view of the demoapp!")
+    path  = request.path
+    method = request.method
+    scheme = request.scheme
+    address = request.META['REMOTE_ADDR']
+    user_agent = request.META['HTTP_USER_AGENT']
+    content = f"""<center><h2>Testing Django Request Response Objects</h2> 
+<p>Request path : " {path}</p> 
+<p>Request Method :{method}</p></center> 
+<p>Request scheme :{scheme}</p></center> 
+<p>Request address :{address}</p></center> 
+<p>Request user_agent :{user_agent}</p></center> """
+    return HttpResponse(content, content_type='text/html',charset='utf-8')
